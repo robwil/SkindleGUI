175ad3f75919eca4314f4ebb48de32622e5638c1
Rob Williams <granddemon@gmail.com>
2011-03-09 11:23:38 -0500

    Added handler for unhandled exceptions at app level

--- SkindleGUI/Program.cs
+++ SkindleGUI/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace SkindleGUI
 {
@@ -15,7 +16,23 @@ namespace SkindleGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            // add handlers for any unhandled exceptions -- credit to http://www.csharp-examples.net/catching-unhandled-exceptions/
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
             Application.Run(new frmMain());
         }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Unhandled Thread Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show((e.ExceptionObject as Exception).Message, "Unhandled UI Exception", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
     }
 }
